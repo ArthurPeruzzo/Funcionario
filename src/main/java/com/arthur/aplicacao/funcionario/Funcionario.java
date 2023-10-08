e5dd1d8049106737b422e92f6b0cf3e1b0e0cf54
ArthurPeruzzo <arthur.kohl@viasoft.com.br>
2023-10-08 20:30:29 -0300

    NMTZ-1 - ajuste

--- src/main/java/com/arthur/aplicacao/funcionario/Funcionario.java
+++ src/main/java/com/arthur/aplicacao/funcionario/Funcionario.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 
 @Entity @Builder
 @AllArgsConstructor @NoArgsConstructor
-@Table(name = "funcionario")
+@Table(name = "FUNCIONARIO")
 @Getter @Setter
 @SequenceGenerator(name="funcionario_seq", sequenceName="funcionario_seq", allocationSize=1)
 public class Funcionario {
@@ -21,43 +21,50 @@ public class Funcionario {
     @Id
     @ApiModelProperty("Identificador")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="funcionario_seq")
-    @Column(name = "funcionarioid")
+    @Column(name = "ID_FUNCIONARIO")
     private Long id;
 
     @Size(max = 30, min = 2, message = "O nome deve possuir entre 2 e 30 caracteres")
     @ApiModelProperty("Nome")
     @Pattern(regexp = "^[A-Z]+(.)*", message = "O nome deve começar com letra maiúscula")
+    @Column(name = "NOME")
     @NotNull
     private String nome;
 
     @CPF(message = "CPF inválido!")
     @ApiModelProperty("CPF")
+    @Column(name = "CPF")
     @NotNull
     private String cpf;
 
     @ApiModelProperty("Telefone")
+    @Column(name = "TELEFONE")
     private String telefone;
 
     @ApiModelProperty("Email")
     @Email(message = "Email inválido!")
+    @Column(name = "EMAIL")
     @NotNull
     private String email;
 
     @ApiModelProperty("Data de admissão")
     @NotNull
-    @Column(name = "dataadmissao")
+    @Column(name = "DATA_ADMISSAO")
     private LocalDate dataAdmissao;
 
     @ApiModelProperty("Departamento")
     @NotNull
+    @Column(name = "DEPARTAMENTO")
     @Enumerated(EnumType.STRING)
     private Departamento departamento;
 
     @ApiModelProperty("Salário")
+    @Column(name = "SALARIO")
     @NotNull
     private BigDecimal salario;
 
     @ApiModelProperty("Status")
+    @Column(name = "STATUS")
     @NotNull
     @Enumerated(EnumType.STRING)
     private Status status;
